9979e0bb9c0aa6372fbda6381365a40c652dd963
suvetha <suvethaj07@gmail.com>
2026-07-28 19:44:50 +0530

    Time: 0 ms (100%), Space: 43.2 MB (16.29%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -0,0 +1,16 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        HashMap<Character,Integer>  map = new HashMap<>();
+
+        for(int i  = 0 ; i < s.length() ; i++){
+            char ch = s.charAt(i);
+
+            if(map.containsKey(ch)){
+                return ch;
+            }else{
+                map.put(ch,1);
+            }
+        }
+        return ' ';
+    }
+}

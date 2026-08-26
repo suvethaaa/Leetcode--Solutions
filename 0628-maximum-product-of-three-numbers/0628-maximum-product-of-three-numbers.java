a9e0463ce2f30bf71301da1e45bdc9cb426fd690
suvetha <suvethaj07@gmail.com>
2026-08-26 11:48:19 +0530

    Time: 2 ms (99.5%), Space: 47 MB (97.24%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -1,24 +1,34 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-        insertionSort(nums);
-        int n = nums.length;
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
 
-        if(nums[0]*nums[1]*nums[n-1] > nums[n-1]*nums[n-2]*nums[n-3]){
-            return nums[0]*nums[1]*nums[n-1];
-        }
-        return nums[n-1]*nums[n-2]*nums[n-3];
-    }
 
-    static void insertionSort(int[] nums){
+        for(int n : nums){
+            if(n > max1){
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            }else if(n > max2){
+                max3 = max2;
+                max2 = n;
+            }else if(n > max3){
+                max3 = n;
+            }
 
-        for(int i = 0 ; i  < nums.length-1 ; i++){
-            for(int j = i+1 ; j > 0 ; j--){
-                if(nums[j] < nums[j-1]){
-                    int temp = nums[j];
-                    nums[j] = nums[j-1];
-                    nums[j-1] = temp;
-                }
+            if(n < min1){
+                min2 = min1;
+                min1 = n;
+            }else if(n < min2){
+                min2 = n;
             }
         }
+
+        return Math.max(max1*max2*max3 , min1*min2*max1);
     }
+
+    
 }

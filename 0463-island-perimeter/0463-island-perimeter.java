a46b81ed6c8f1daff01b69d410044e9c6351590e
suvetha <suvethaj07@gmail.com>
2026-09-16 09:11:15 +0530

    Time: 5 ms (38.12%), Space: 47.1 MB (82.67%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -8,42 +8,25 @@ class Solution {
 
                 if(grid[i][j] == 1){
                     //check up
-                    if(i == 0){
+                    if(i == 0 || grid[i-1][j] == 0){
                         pm++;
-                    }
-                    else {
-                        if(grid[i-1][j] == 0){
-                            pm++;
-                        }
                     }
                     
                     //check down 
-                    if(i == grid.length-1){
+                    if(i == grid.length-1 || grid[i+1][j] == 0){
                         pm++;
                     }
-                    else {
-                        if(grid[i+1][j] == 0){
-                            pm++;
-                        }
-                    }
+                    
                     //check left 
-                    if(j == 0){
+                    if(j == 0 || grid[i][j-1] == 0){
                         pm++;
                     }
-                    else{
-                        if(grid[i][j-1] == 0){
-                            pm++;
-                        }
-                    }
+                    
                     //check right
-                    if(j == grid[0].length -1){
+                    if(j == grid[0].length -1 || grid[i][j+1] == 0){
                         pm++;
                     }
-                    else{
-                        if(grid[i][j+1] == 0){
-                            pm++;
-                        }
-                    }
+                    
                 }
             }
         }
